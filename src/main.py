c6a4b681c00cf9418edbadc113bb659ee99e08d7
Oliver Cordes <ocordes@astro.uni-bonn.de>
2024-01-21 15:47:12 +0100

    - add argument parser, handle messages

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # main.py 
 #
 # written by: Oliver Cordes 2024-01-20
-# changed by: Oliver Cordes 2024-01-20
+# changed by: Oliver Cordes 2024-01-21
 
 
 from dotenv import load_dotenv
@@ -9,13 +9,21 @@ import os, sys
 
 import requests
 
+import argparse
+
+__version__ = '0.99.0'
+
 # load .env file
 load_dotenv()
 
 
 def get_main_power():
+    """
+    Get the current power from the main power source    
+    """
     power_type = os.getenv('MAIN_POWER')
     
+    msg = 'OK'
     if power_type == 'tasmota':
 
         BASE_URL = os.getenv('TASMOTA_URL')
@@ -24,38 +32,43 @@ def get_main_power():
         try:
             response = requests.get(URL,timeout=5)
         except requests.exceptions.Timeout:
-            print('Error: Could not get power data (timeout)')
-            return None 
+            msg = 'Error: Could not get power data (timeout)'
+            return None, msg
         
 
         if response.status_code != 200:
-            print(f'Error: Could not get power data (error={response.status_code})')
-            return None
+            msg = f'Error: Could not get power data (error={response.status_code})'
+            return None, msg
         
         data = response.json()
         power = data['StatusSNS']['ENERGY']['Power_cur']
 
-        return power
+        return power, msg
     else:
-        return None
+        msg = f'Error: Unknown power type {power_type}'
+        return None, msg
 
 
 def ahoy_get_power_limit():
-
+    """
+    Get the current power limit from the ahoy DTU server
+    """
     AHOY_SERVER = os.getenv('AHOY_DTU_URL')
     INVERTER = os.getenv('AHOY_DTU_INVERTER')
 
     URL = f'{AHOY_SERVER}/api/inverter/id/{INVERTER}'
     
+    msg = 'OK'
+
     try:
         response = requests.get(URL,timeout=5)
     except requests.exceptions.Timeout:
-        print('Error: Could not get power data (timeout)')
-        return None 
+        msg = 'Error: Could not get power data (timeout)'
+        return None, None, msg 
 
     if response.status_code != 200:
-        print(f'Error: Could not get power data (error={response.status_code})')
-        return None
+        msg = f'Error: Could not get power data (error={response.status_code})'
+        return None, None,  msg
 
     # convert data
     data = response.json()
@@ -64,15 +77,17 @@ def ahoy_get_power_limit():
     power = int(data['ch'][0][2])
 
     limit = 0
+    max_power = int(data['max_pwr'])
+
+
     if data['power_limit_ack'] and (data['power_limit_read'] < 65000):
-        max_power = int(data['max_pwr'])
         limit_read = int(data['power_limit_read']) / 100
         limit = int(max_power * limit_read) 
 
     if limit > power:
         limit = power
 
-    return limit
+    return limit, max_power, msg
 
 
 def ahoy_set_power_limit(limit):
@@ -96,29 +111,68 @@ def ahoy_set_power_limit(limit):
     return
 
 
+def doit(args):
+    mp, error_msg = get_main_power()
+
+    if mp is None:
+        print('No main power defined: {error_msg}') 
+        sys.exit(1)
+
+    print(f'current power consumption: {mp} W')
+
+    power_limit, max_power, error_msg = ahoy_get_power_limit()
+
+    if power_limit is None:
+        print('No power limit defined: {error_msg}') 
+        sys.exit(1)
+
+    print(f'current inverter power:    {power_limit} W  (max power: {max_power} W)')
+
+
+    if mp > 0:
+        # no energy is served to the grid
+
+        if int(os.getenv('MAX_VALUE')) > 0:
+            new_limit = int(os.getenv('MAX_VALUE'))
+        else:
+            if args.maxpower > 0:
+                new_limit = args.maxpower
+            else:
+                new_limit = max_power
+
+    else:
+        # energy is served to the grid
+
+        zero = int(os.getenv('ZERO'))
+        # calculate the limit for zeroenergy
+        new_limit = mp + power_limit - zero
+
+        # if we have a negative value, serve no energy to the grid
+        if new_limit < 0:
+            new_limit = 0   
+
+
+    print(f'new inverter limit:        {new_limit} W') 
+
+    #ahoy_set_power_limit(400)
+    #ahoy_set_power_limit(new_limit)
+
+
 # main
-mp = get_main_power()
 
-if mp is None:
-    print('No main power defined') 
-    sys.exit(1)
-
-print(mp)
-
-power_limit = ahoy_get_power_limit()
-
-print(power_limit)
+parser = argparse.ArgumentParser(
+    prog='zeroenergy',    
+    description='Implementing zero power to the power grid (Nulleinspeisung)',
+    epilog='(C) 2024 Oliver Cordes')
 
 
-new_limit = mp + power_limit + 5
+parser.add_argument('-v', '--verbose',
+                    action='store_true')  # on/off flag
+parser.add_argument('--version', action='version', version=f'%(prog)s {__version__} (C) 2024 Oliver Cordes')
+parser.add_argument('-m', '--maxpower', action='store', type=int, default=-1)                   
+
+args = parser.parse_args()
+
+doit(args)
 
 
-print(new_limit)
-
-if new_limit < 0:
-    new_limit = 0
-
-print('new limit: ', new_limit) 
-
-ahoy_set_power_limit(400)
-#ahoy_set_power_limit(new_limit)
